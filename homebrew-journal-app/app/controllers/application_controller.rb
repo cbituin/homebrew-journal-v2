abc2fdaa05fd1a2d55382ced676db5331454d5a3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-22 04:44:59 +0000

    Adds spec.md, additional gem and related notes

--- homebrew-journal-app/app/controllers/application_controller.rb
+++ homebrew-journal-app/app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 require './config/environment'
+require 'rack-flash'
+
+# To display this message in the view, just add
+#<!-- views/file.erb -->
+# <% if flash.has?(:message) %>
+#   <%= flash[:message] %>
+# <% end %>
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
